f452a81ff6480262d7dd0c3425fa8e7e6d28c6a8
Jeremiah Pope <thepeabear@gmail.com>
2023-06-09 11:26:22 -0500

    code was working but changed location and now have issues againadded more questions and added a few more index elements

--- assets/logic.js
+++ assets/logic.js
@@ -1,2 +1,50 @@
+const questions = require("./questions");
 const timerEl = document.querySelector("#timer");
 const scoresEl = document.querySelector("#scores");
+const testBtn = document.getElementById("test-btn");
+const questionsContainer = document.getElementById("questions-container");
+const choicesContainer = document.getElementById("choices-container");
+const answersContainer = document.getElementById("answers-container");
+
+testBtn.addEventListener("click", function () {
+  getNextQuestion();
+});
+
+let currentIndex = 0;
+
+function getNextQuestion() {
+  const currentQuestion = questions[currentIndex];
+
+  const questionTextEl = document.createElement("p");
+  questionTextEl.textContent = "Question: " + currentQuestion.question;
+  questionsContainer.appendChild(questionTextEl);
+
+  const choicesTextEl = document.createElement("p");
+  choicesTextEl.textContent = "Choices: " + currentQuestion.choices.join(", ");
+  choicesContainer.appendChild(choicesTextEl);
+
+  const answersTextEl = document.createElement("p");
+  answersTextEl.textContent = "Answers: " + currentQuestion.answers;
+  answersContainer.appendChild(answersTextEl);
+
+  currentIndex++;
+
+  // Remove previous question
+  setTimeout(() => {
+    questionsContainer.removeChild(questionTextEl);
+    choicesContainer.removeChild(choicesTextEl);
+    answersContainer.removeChild(answersTextEl);
+
+    if (currentIndex < questions.length) {
+      // Display next question
+      getNextQuestion();
+    } else {
+      // Display end message when all questions have been answered
+      const endMessageEl = document.createElement("p");
+      endMessageEl.textContent = "All questions answered!";
+      questionsContainer.appendChild(endMessageEl);
+    }
+  }, 5000); // Change this to however long you want to display each question
+
+  return currentQuestion;
+}
--- assets/questions.js
+++ assets/questions.js
@@ -1,83 +1,89 @@
-const testBtn = document.getElementById("test-btn");
 const testQuestions = [
   {
     question:
       "#1: What is the output of the following code:\n\nvar x = 5;\n\nvar y = “5”;\n\nconsole.log(x == y);",
-    choices: ["true", "false", "undefined", "Error"],
-    answer: 0, // Index of the correct answer within the choices array
+    choices: ["a): true", "b): false", "c): undefined", "d): Error"],
+    answers: 0, // Index of the correct answer within the choices array
   },
   {
-    question: "What does the “this” keyword refer to in JavaScript?",
+    question: "#2: What does the “this” keyword refer to in JavaScript?",
     choices: [
-      "The current function",
-      "The global object",
-      "The object that the function belongs to",
-      "The parent object of the current object",
+      "a): The current function",
+      "b): The global object",
+      "c): The object that the function belongs to",
+      "d): The parent object of the current object",
     ],
     answer: 2, // Index of the correct answer within the choices array
   },
   {
-    question: "What is the correct syntax for a “for” loop in JavaScript?",
+    question: "#3: What is the correct syntax for a “for” loop in JavaScript?",
     choices: [
-      "for (var i = 0; i < 5; i++)",
-      "for (i = 0; i < 5; i++)",
-      "for (var i = 5; i > 0; i–)",
-      "for (i = 5; i > 0; i–)",
+      "a): for (var i = 0; i < 5; i++)",
+      "b): for (i = 0; i < 5; i++)",
+      "c): for (var i = 5; i > 0; i–)",
+      "d): for (i = 5; i > 0; i–)",
     ],
     answer: 0, // Index of the correct answer within the choices array
   },
   {
     question:
-      "What is the difference between “==” and “===” operators in JavaScript?",
+      "#4: What is the difference between “==” and “===” operators in JavaScript?",
     choices: [
-      "They are interchangeable",
-      "“==” performs a strict comparison, while “===” performs a loose comparison",
-      "“===” performs a strict comparison, while “==” performs a loose comparison",
-      "They both perform the same type of comparison",
+      "a):They are interchangeable",
+      "b):“==” performs a strict comparison, while “===” performs a loose comparison",
+      "c):“===” performs a strict comparison, while “==” performs a loose comparison",
+      "d):They both perform the same type of comparison",
     ],
     answer: 2, // Index of the correct answer within the choices array
   },
   {
-    question: "Which of the following is not a data type in JavaScript?",
-    choices: ["Boolean", "String", "Number", "Character"],
+    question: "#5: Which of the following is not a data type in JavaScript?",
+    choices: ["a):Boolean", "b):String", "c):Number", "d):Character"],
     answer: 3, // Index of the correct answer within the choices array
   },
   {
     question:
-      "Which of the following is not a valid way to declare a function in JavaScript?",
+      "#6: Which of the following is not a valid way to declare a function in JavaScript?",
     choices: [
-      "function myFunction() {}",
-      "var myFunction = function() {}",
-      "() => {}",
-      "function = {}",
+      "a):function myFunction() {}",
+      "b):var myFunction = function() {}",
+      "c):() => {}",
+      "d):function = {}",
     ],
     answer: 3, // Index of the correct answer within the choices array
   },
   {
-    question: "Which of the following is not a loop in JavaScript?",
-    choices: ["for", "while", "do...while", "next"],
+    question: "#7: Which of the following is not a loop in JavaScript?",
+    choices: ["a):for", "b):while", "c):do...while", "d):next"],
     answer: 3, // Index of the correct answer within the choices array
   },
   {
     question:
-      "What is the output of the following code: console.log(“5” == 5);",
-    options: ["true", "false", "Error", "NaN"],
+      "#8: What is the output of the following code: console.log(“5” == 5);",
+    options: ["a):true", "b):false", "c):Error", "d):NaN"],
     answer: 0, //true
   },
+  {
+    question:
+      "#9: What is the difference between “let” and “const” keywords in JavaScript?",
+    options: [
+      "a): They are interchangeable",
+      "b): “let” variables cannot be reassigned, while “const” variables can",
+      "c): “const” variables cannot be reassigned, while “let” variables can",
+      "d): “let” and “const” both refer to constant variables",
+    ],
+    answer: "c",
+  },
+  {
+    question:
+      "#10: Which of the following is not a comparison operator in JavaScript?",
+    options: ["a) ==", "b) ===", "c) !=", "d) =<"],
+    answer: "d",
+  },
+  {
+    question:
+      "#11: What is the output of the following code?\nvar x = 5;\nconsole.log(x++);",
+    options: ["a) 4", "b) 5", "c) 6", "d) Error"],
+    answer: "b",
+  },
 ];
-testBtn.addEventListener("click", function () {
-  getNextQuestion();
-});
-let currentIndex = 0;
-function getNextQuestion() {
-  const currentQuestion = testQuestions[currentIndex];
-  currentIndex++;
-
-  const questionsContainer = document.getElementById("questions-container");
-
-  const questionTextEl = document.createElement("p");
-  questionTextEl.textContent = currentQuestion.question;
-  questionsContainer.appendChild(questionTextEl);
-
-  return currentQuestion;
-}
